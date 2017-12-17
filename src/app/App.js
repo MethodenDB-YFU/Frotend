52ccb0894a3227e5823a821b37b2cc38a8003b13
Marco Heumann (mheumann) <marco.heumann@posteo.de>
2017-12-17 20:03:12 +0100

    first configurations

---
+++ src/app/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Layout, Menu } from 'antd';
+import './App.css';
+const {Header, Footer, Sider, Content} = Layout;
+
+class App extends Component {
+    render() {
+        return (
+                <Layout>
+                    <Header>
+                        <div className="logo" />
+                        <Menu
+                            theme="dark"
+                            mode="horizontal"
+                            defaultSelectedKeys={['1']}
+                            >
+                            <Menu.Item key="1">nav 1</Menu.Item>
+                            <Menu.Item key="2">nav 2</Menu.Item>
+                            <Menu.Item key="3">nav 3</Menu.Item>
+                        </Menu>
+                    </Header>
+                    <Layout>
+                        <Sider>Sider</Sider>
+                        <Content>Content</Content>
+                    </Layout>
+                    <Footer>Footer</Footer>
+                </Layout>
+                );
+    }
+}
+;
+
+export default App;
