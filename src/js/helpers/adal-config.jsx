c4a4b6927617f0f24b79b0cb64cf449165a0c0d9
Thro42 <thro@thomitzek.com>
2018-07-24 18:36:03 +0200

    Use YFU AD
    Nutzung des YFU AzureAD

--- src/js/helpers/adal-config.jsx
+++ src/js/helpers/adal-config.jsx
@@ -1,9 +1,11 @@
 import AuthenticationContext from 'adal-angular';
 
+//tenant: '90d6040d-e0e5-4b66-b149-4b2328b5f563',
+//clientId: '8e64138a-e22c-40b1-b782-25e26bbe952d',
 var adalConfig = {
     instance: 'https://login.microsoftonline.com/',
-    tenant: '90d6040d-e0e5-4b66-b149-4b2328b5f563',
-    clientId: '8e64138a-e22c-40b1-b782-25e26bbe952d',
+    tenant: '3496a242-cb50-40ae-9566-623507dd3c89',
+    clientId: 'bb17b83a-276f-4391-8199-1b75661b63e3',
     extraQueryParameter: 'nux=1',
     disableRenewal: true,
     //cacheLocation: 'localStorage',
